a5b08b4ac8aefa73fca46b14b60bb300b5ae251e
Tolpa-999 <mahmudtolpa666@gmail.com>
2025-01-28 21:53:55 +0200

    fix index.js cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require("cors")
 const express = require('express');
 const app = express();
 app.use(cors({
-    origin: 'https://perfumeni.vercel.app/', // Replace with your frontend's URL
+    origin: 'https://perfumeni.vercel.app', // Replace with your frontend's URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
     credentials: true, // Allow cookies if needed
   }))
